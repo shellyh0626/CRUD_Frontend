ab2719c435fc1a17e26d1684d341f3432a578194
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 13:47:26 -0400

    Created a thunk for fetchAllCampuses

--- src/redux/campus/campus.actions.js
+++ src/redux/campus/campus.actions.js
@@ -9,3 +9,16 @@ export const fetchAllCampuses = (payload) => {
     payload: payload,
   };
 };
+
+export const fetchAllCampusesThunk = () => {
+  return async (dispatch) => {
+    try {
+      console.log("FETCH ALL CAMPUSES THUNK IS FIRING");
+      const response = await axios.get("http://localhost:8080/api/students");
+      console.log("FETCH ALL CAMPUSES THUNK COMPLETED");
+      dispatch(fetchAllCampuses(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
