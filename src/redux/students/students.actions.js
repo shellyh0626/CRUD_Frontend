d562ec517281604ed0c8f975cc3adefbcba2f096
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 16:59:46 -0400

    Added action for fetch student by id

--- src/redux/students/students.actions.js
+++ src/redux/students/students.actions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import StudentActionType from "./students.types";
 
 export const fetchAllStudents = (payload) => {
-  console.log("FETCH ALL STUDENTs ACTION");
+  //   console.log("FETCH ALL STUDENTs ACTION");
   return {
     type: StudentActionType.FETCH_ALL_STUDENTS,
     payload: payload,
@@ -13,12 +13,19 @@ export const fetchAllStudents = (payload) => {
 export const fetchAllStudentsThunk = () => {
   return async (dispatch) => {
     try {
-      console.log("FETCH ALL STUDENTS THUNK IS FIRING");
+      //   console.log("FETCH ALL STUDENTS THUNK IS FIRING");
       const response = await axios.get("http://localhost:8080/api/students");
-      console.log("FETCH ALL STUDENTS THUNK COMPLETED");
+      //   console.log("FETCH ALL STUDENTS THUNK COMPLETED");
       dispatch(fetchAllStudents(response.data));
     } catch (error) {
       console.error(error);
     }
+  };
+};
+
+export const fetchStudentById = (payload) => {
+  return {
+    type: StudentActionType.FETCH_STUDENT_BY_ID,
+    payload: payload,
   };
 };
--- src/redux/students/students.types.js
+++ src/redux/students/students.types.js
@@ -1,5 +1,6 @@
 const StudentActionType = {
   FETCH_ALL_STUDENTS: "FETCH_ALL_STUDENTS",
+  FETCH_STUDENT_BY_ID: "FETCH_STUDENT_BY_ID",
 };
 
 export default StudentActionType;
