14cbbed2caac81b77d9d2391b9e83653cbdd596c
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 17:31:32 -0400

    Added update campus type

--- src/redux/campuses/campuses.types.js
+++ src/redux/campuses/campuses.types.js
@@ -2,6 +2,7 @@ const CampusActionType = {
   FETCH_ALL_CAMPUSES: "FETCH_ALL_CAMPUSES",
   FETCH_CAMPUS_BY_ID: "FETCH_CAMPUS_BY_ID",
   ADD_CAMPUS: "ADD_CAMPUS",
+  UPDATE_CAMPUS: "UPDATE_CAMPUS",
 };
 
 export default CampusActionType;
