6000f2ef5c789d637fcaf4ef1a0e9bcd8a314ac5
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 17:11:36 -0400

    Added an action type called add campus

--- src/redux/campuses/campuses.types.js
+++ src/redux/campuses/campuses.types.js
@@ -1,6 +1,7 @@
 const CampusActionType = {
   FETCH_ALL_CAMPUSES: "FETCH_ALL_CAMPUSES",
   FETCH_CAMPUS_BY_ID: "FETCH_CAMPUS_BY_ID",
+  ADD_CAMPUS: "ADD_CAMPUS",
 };
 
 export default CampusActionType;
