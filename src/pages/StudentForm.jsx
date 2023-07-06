3c4a1f3194c7670d724a81d140c348af7c6bfd37
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 23:26:55 -0400

    Added a basic structure to student form

--- src/pages/StudentForm.jsx
+++ src/pages/StudentForm.jsx
@@ -4,6 +4,35 @@ const StudentForm = () => {
   return (
     <div className="container">
       <h1>StudentForm Page</h1>
+      <div className="container">
+        <h1>CampusForm Page</h1>
+        <div>
+          <div>
+            <label>campus</label>
+            <select></select>
+          </div>
+          <div>
+            <label>first name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>last name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>email</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>image</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>gpa</label>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
