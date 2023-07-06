0cfb8f184322753237ce75d6d63f930efef35c5b
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 23:29:04 -0400

    Added a basic structure for storing details about a specific campus and students in that campus

--- src/pages/Campus.jsx
+++ src/pages/Campus.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-// Just my home page
+import ListStudents from "../components/ListStudents";
 
 const Campus = () => {
   return (
     <div className="container">
       <h1>Campus Page</h1>
+      <div>
+        <img src="" />
+        <p>name: </p>
+        <p>address: </p>
+        <p>description</p>
+      </div>
+      <div>
+        <ListStudents />
+      </div>
     </div>
   );
 };
