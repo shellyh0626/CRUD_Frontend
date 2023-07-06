fbf214a6e4401ce3e479390ef75cd5985f233f73
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 22:32:51 -0400

    Added useSelector method to access student data from store

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ListStudents from "../components/ListStudents";
 
 const Students = () => {
+  const allStudents = useSelector((state) => state.students.allStudents);
+
   return (
     <div className="container">
-      <h1>Students Page</h1>
+      <h1>
+        Students Page &nbsp; <Link to="/students/new">Create Student</Link>
+      </h1>
+      <ListStudents list={allStudents} />
     </div>
   );
 };
