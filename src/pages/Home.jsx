cbab643fa9f719ef63a48f208c781e96f5d0dbb2
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 20:36:22 -0400

    Added a sample home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,39 @@
 import React from "react";
-// Just my home page
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link to="/students">Students</Link>
+        </li>
+        <li>
+          <Link to="/campuses">Campuses</Link>
+        </li>
+      </ul>
+      <p>Description:</p>
+      <p>
+        <li>Student Number: </li>
+        <li>Student Name: </li>
+      </p>
+      <p>Background API:</p>
+      <ul>
+        <li>GET /api/campuses/</li>
+        <li>POST /api/campuses/</li>
+        <li>GET /api/campuses/:id</li>
+        <li>PUT /api/campuses/:id</li>
+        <li>DELETE /api/campuses/:id</li>
+      </ul>
+      <ul>
+        <li>GET /api/students/</li>
+        <li>POST /api/students/</li>
+        <li>GET /api/students/campus/:id</li>
+        <li>GET /api/students/:id</li>
+        <li>PUT /api/students/:id</li>
+        <li>DELETE /api/students/:id</li>
+      </ul>
     </div>
   );
 };
