cfabf55d42bb811e829ee94dfa860c224a7e9a20
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 23:20:21 -0400

    Created a basic structure for campus form

--- src/pages/CampusForm.jsx
+++ src/pages/CampusForm.jsx
@@ -4,6 +4,24 @@ const CampusForm = () => {
   return (
     <div className="container">
       <h1>Campuses Page</h1>
+      <div>
+        <div>
+          <label>name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>image</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>description</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>address</label>
+          <input type="text" />
+        </div>
+      </div>
     </div>
   );
 };
