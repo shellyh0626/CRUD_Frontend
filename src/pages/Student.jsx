a4936a293bdf27c44612f29e89c63dd674f85231
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 23:33:59 -0400

    Added a basic structure for storing details about a single student

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -4,6 +4,21 @@ const Student = () => {
   return (
     <div className="container">
       <h1>Student Page</h1>
+      <div>
+        <h2>Student Info</h2>
+        <img src="" />
+        <p>first name: </p>
+        <p>last name: </p>
+        <p>email</p>
+        <p>gpa</p>
+      </div>
+      <div>
+        <h2>Campuse Info</h2>
+        <img src="" />
+        <p>name: </p>
+        <p>address: </p>
+        <p>description</p>
+      </div>
     </div>
   );
 };
